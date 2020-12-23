59ebf7568c4713c2ec1d8841b2d1492fa36bcc76
qiaojialin <646274302@qq.com>
2020-12-23 19:28:47 +0800

    make public

---
+++ correctiontest/src/test/java/org/apache/iotdb/client/SingleClientTest.java
@@ -0,0 +1,18 @@
+package org.apache.iotdb.client;
+
+import org.junit.Test;
+
+public class SingleClientTest {
+
+  @Test
+  public void testGenerateSql() {
+    SingleClient singleClient = new SingleClient();
+    System.out.println(singleClient.generateRawDataQuerySql(true, true, 0));
+    System.out.println(singleClient.generateRawDataQuerySql(true, false, 0));
+    System.out.println(singleClient.generateRawDataQuerySql(false, true, 0));
+    System.out.println(singleClient.generateCountAggregatedQuerySql(true, true, 0));
+    System.out.println(singleClient.generateCountAggregatedQuerySql(false, true, 0));
+    System.out.println(singleClient.generateCountAggregatedQuerySql(true, false, 0));
+  }
+
+}

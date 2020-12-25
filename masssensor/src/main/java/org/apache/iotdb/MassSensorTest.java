0cd8e7ee4a4ea57d63031462f37ecbe692b73f06
151250176 <151250176@smail.nju.edu.cn>
2020-12-25 15:23:26 +0800

    change ip

--- masssensor/src/main/java/org/apache/iotdb/MassSensorTest.java
+++ masssensor/src/main/java/org/apache/iotdb/MassSensorTest.java
@@ -26,7 +26,7 @@ public class MassSensorTest {
      */
     public static void main(String[] args) {
         if (args == null || args.length == 0) {
-            args = new String[]{"192.168.130.6", "6667", "root", "root", "50"};
+            args = new String[]{"127.0.0.1", "6667", "root", "root", "50"};
         }
         sessionPool = new SessionPool(args[0], Integer.parseInt(args[1]), args[2], args[3], Integer.parseInt(args[4]));
 
